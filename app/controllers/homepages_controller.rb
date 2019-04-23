0b488d197484916f10ac16be3ff2f55dd1091313
Jansen <v.jansen.martin@gmail.com>
2019-04-22 17:24:43 -0700

    Add functionality to sample from each media category inside the homepages controller.  Render the samples (and a media spotlight second) through view

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,11 @@
 class HomepagesController < ApplicationController
   def index
-    @num_of_works = Work.all.count
+    @works = Work.all
+
+    @media_spotlight = @works.sample
+
+    @top_movies = @works.where(category: "movie").sample(10)
+    @top_books = @works.where(category: "book").sample(10)
+    @top_albums = @works.where(category: "album").sample(10)
   end
 end
