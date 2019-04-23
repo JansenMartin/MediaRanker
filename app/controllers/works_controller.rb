0e60f4ae119ddd5d5d63e2a6c84305450a0e95a0
Jansen <v.jansen.martin@gmail.com>
2019-04-23 16:12:07 -0700

    Add works#show action; Add work#show route; Add work#show test.  All tests currently passing

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,16 @@ class WorksController < ApplicationController
     @books = Work.where(category: "book")
     @movies = Work.where(category: "movie")
   end
+
+  def show
+    # Grab the id of work we wanna show
+    work_id = params[:id]
+    # Use that id to find work object, assign it a name
+    @work = Work.find_by(id: work_id)
+
+    # If the work doesn't exist, not found!
+    unless @work
+      head :not_found
+    end
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -10,4 +10,20 @@ describe WorksController do
     # Assert
     must_respond_with :ok
   end
+
+  describe "show" do
+    it "returns a 404 status if the work doesn't exist" do
+      # Arrange
+      bogus_work_id = 9001
+
+      # Act
+      get work_path(bogus_work_id)
+
+      # Assert
+      must_respond_with :not_found
+    end
+
+    it "works for a work that exists" do
+    end
+  end
 end
