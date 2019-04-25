f71a9f9e93400e90a11f8f178e8064d692e82376
Jansen <v.jansen.martin@gmail.com>
2019-04-24 18:47:52 -0700

    Add tests for index_media helper method

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -8,4 +8,30 @@ describe Work do
   it "passes validations with good data" do
     expect(@work).must_be :valid?
   end
+
+  describe "index_media" do
+    it "returns a collection of works with the same category type" do
+      # Arrange / Act
+      movie_collection = Work.index_media("movie")
+      book_collection = Work.index_media("book")
+      album_collection = Work.index_media("album")
+
+      # Assert
+      expect(movie_collection.count).must_equal Work.where(category: "movie").count
+      expect(book_collection.count).must_equal Work.where(category: "book").count
+      expect(album_collection.count).must_equal Work.where(category: "album").count
+    end
+
+    it "returns an empty list if there are no works in the database" do
+      Work.destroy_all
+
+      movie_collection = Work.index_media("movie")
+      book_collection = Work.index_media("book")
+      album_collection = Work.index_media("album")
+
+      expect(movie_collection.count).must_equal 0
+      expect(book_collection.count).must_equal 0
+      expect(album_collection.count).must_equal 0
+    end
+  end
 end
