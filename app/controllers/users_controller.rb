494255ea15fe8128124a8bb3c591c34cb703b2f7
Jansen <v.jansen.martin@gmail.com>
2019-04-25 15:54:22 -0700

    Add login_form action to Users controller; add corresponding view and test.  All tests currently passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  # Has this user logged in before?
+  # If yes, set the session to the user id...and welcome them!
+  # If no, create a new user...THEN set the session to user id, and welcome them!
+
+  # What if there's an error?
+  # Redirect to login_form
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,16 @@ describe UsersController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  describe "login_form" do
+    it "renders without crashing" do
+      # Arrange
+
+      # Act
+      get login_path
+
+      # Assert
+      must_respond_with :ok
+    end
+  end
 end
