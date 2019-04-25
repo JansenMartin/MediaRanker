40c125554b3d74b7a278cba2241758154b3ee05d
Jansen <v.jansen.martin@gmail.com>
2019-04-25 12:51:40 -0700

    Remove validations for creator and category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,12 +1,6 @@
 class Work < ApplicationRecord
-  # Must have a category
-  validates :category, presence: true
-
   # Must have a title
   validates :title, presence: true
-
-  # Must have a creator
-  validates :creator, presence: true
 
   def self.index_media(media_type)
     collection = Work.where(category: media_type)
