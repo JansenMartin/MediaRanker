4310a568eedf8e8f8553db296411a5cea2eb971b
Jansen <v.jansen.martin@gmail.com>
2019-04-24 19:20:12 -0700

    Add tests for top_media helper model method

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -34,4 +34,35 @@ describe Work do
       expect(album_collection.count).must_equal 0
     end
   end
+
+  describe "top_media" do
+    before do
+      @top_movies = Work.top_media("movie")
+    end
+    it "returns a list of 10 Work objects" do
+      movie = @top_movies.first
+
+      expect(movie).must_be_kind_of Work
+      expect(@top_movies.count).must_equal 10
+    end
+
+    it "returns a list of 10 movies, even if there are more than 10 of them" do
+      movies_count = Work.where(category: "movie").count
+
+      expect(@top_movies.count).must_equal 10
+      expect(movies_count > 10).must_equal true
+    end
+
+    it "returns an empty list if there are no works in the database" do
+      Work.destroy_all
+
+      top_movies = Work.top_media("movie")
+      top_books = Work.top_media("book")
+      top_albums = Work.top_media("album")
+
+      expect(top_movies.count).must_equal 0
+      expect(top_books.count).must_equal 0
+      expect(top_albums.count).must_equal 0
+    end
+  end
 end
