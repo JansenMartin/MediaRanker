f03bac5783544e1c7d21503ed076f27fbcce0192
Jansen <v.jansen.martin@gmail.com>
2019-04-27 14:31:22 -0700

    Add number of votes to works page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,19 @@
 class VotesController < ApplicationController
-  #   def create
-  #     puts "*****#{session[:user_id]}"
-  #     user = User.find_by(id: session[:user_id])
-  #     work = Work.find_by(id: params[:work_id])
+  def create
+    user = User.find_by(id: session[:user_id])
+    work = Work.find_by(id: params[:work_id])
 
-  #     @vote = Vote.new(
-  #       work_id: params[:work_id],
-  #       user_id: user.id,
-  #     )
+    @vote = Vote.new(
+      work_id: params[:work_id],
+      user_id: user.id,
+    )
 
-  #     @vote.save
+    @vote.save
 
-  #     redirect_to root_path
+    redirect_to root_path
 
-  #     # Is a user logged in?
-  #     # Is a user not logged in?
-  #     # What happens if not?
-  #   end
+    # Is a user logged in?
+    # Is a user not logged in?
+    # What happens if not?
+  end
 end
