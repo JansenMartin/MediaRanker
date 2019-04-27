e9c64e7a948359c79576117855ea0510b6980754
Jansen <v.jansen.martin@gmail.com>
2019-04-26 18:18:41 -0700

    Add test verifying a user without a username is invalid.  All tests passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,10 @@ describe User do
   it "must be valid" do
     value(user).must_be :valid?
   end
+
+  it "must reject a user without a username" do
+    user.username = ""
+
+    value(user).must_be :invalid?
+  end
 end
