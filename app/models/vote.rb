6c1eac089ee570bba3eb9c374a2460e9d2d8360c
Jansen <v.jansen.martin@gmail.com>
2019-04-27 15:42:38 -0700

    Add vote_count column to Works table

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
-  belongs_to :work
+  belongs_to :work, counter_cache: true
 
   validates :work_id, uniqueness: { scope: :user_id,
                                     message: "User cannot vote for the same work twice." }
