51ae1ce8528803a96780d84caba5564fda2e966f
Jansen <v.jansen.martin@gmail.com>
2019-04-27 15:16:34 -0700

    Add comments outlining what to test

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -4,4 +4,12 @@ describe VotesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  describe "create" do
+    # It needs to add a vote to the database
+    # It needs to reject a second vote made by the same user
+    # It needs to accept only votes from logged-in users
+    # It needs to add a vote to a particular work
+    # It needs to add a vote to a particular user
+  end
 end
