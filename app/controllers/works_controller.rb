fb66616a65e066d631622699d1f4db76f1298056
Jansen <v.jansen.martin@gmail.com>
2019-04-28 16:50:57 -0700

    Clean up comments.  All tests passing

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -17,53 +17,29 @@ class WorksController < ApplicationController
     successful = @work.save
 
     if successful
-      # Set flash status to success
       flash[:status] = :success
-      # Set flash message
       flash[:message] = "Successfuly created #{@work.category}  #{Work.where(category: @work.category).count}"
-      # Don't use NOW since we redirect
       redirect_to works_path
     else
-      # Set flash.now status to error
       flash.now[:status] = :error
-      # Set flash.now message
       flash.now[:message] = "A problem occurred:  Could not create #{@work.category}"
-      # Use NOW since we RENDER
       render :new, status: :bad_request
     end
   end
 
   def show
-    # # Grab the id of work we wanna show
-    # work_id = params[:id]
-    # # Use that id to find work object, assign it a name
-    # @work = Work.find_by(id: work_id)
-
-    # If the work doesn't exist, not found!
     unless @work
       head :not_found
     end
   end
 
   def edit
-    # # What's the work id?
-    # work_id = params[:id]
-
-    # # What's the work?
-    # @work = Work.find_by(id: work_id)
-
-    # Does this work even exist?
-    # If not, what happens? head :not_found
-
     unless @work
       head :not_found
     end
   end
 
   def update
-    # work_id = params[:id]
-    # @work = Work.find_by(id: work_id)
-
     unless @work
       head :not_found
       return
@@ -81,8 +57,6 @@ class WorksController < ApplicationController
   end
 
   def destroy
-    # Find id - done by filter already
-
     unless @work
       head :not_found
       return
