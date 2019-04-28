26e4e56ea8bff99f07181f79526b9e496c1e52e6
Jansen <v.jansen.martin@gmail.com>
2019-04-28 16:40:27 -0700

    Clean up comments; Add validation test for votes.  All tests passing

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  before do
+    @vote = Vote.first
+  end
 
-  # it "must be valid" do
-  #   value(vote).must_be :valid?
-  # end
+  describe "validations" do
+    it "passes validations with good data" do
+      expect(@vote).must_be :valid?
+    end
+
+    it "rejects an invalid vote, so a user cannot vote for the same work twice" do
+      invalid_vote = Vote.new(user_id: @vote.user_id, work_id: @vote.work_id)
+      result = invalid_vote.save
+      expect(result).must_equal false
+    end
+  end
 end
